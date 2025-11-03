91c00afdf0ce19fd0adca204c8d0e8762773fed5
Kamogelo01 <221325395@edu.vut.ac.za>
2025-11-03 14:59:37 +0200

    Add all files via upload

---
+++ Passenger counter/index.js
@@ -0,0 +1,17 @@
+let countEL = document.getElementById("count-el")
+let saveBtn = document.getElementById("save-btn")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment(){
+  count += 1
+  countEL.innerText = count
+}
+
+function save(){
+  let savedNum = count + " - "
+  saveEl.textContent +=  savedNum
+  countEL.innerText = 0
+  count = 0
+  //console.log(count)
+}
